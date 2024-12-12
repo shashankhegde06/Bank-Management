9a8043d9531149a2f9c16165633b1c5766de4ac2
Shashank Hegde <141432130+shashankhegde06@users.noreply.github.com>
2024-12-12 14:53:41 +0530

    Create LowBalanceException.java

---
+++ src/main/java/com/alien/bank/management/system/exception/LowBalanceException.java
@@ -0,0 +1,11 @@
+package com.alien.bank.management.system.exception;
+
+public class LowBalanceException extends RuntimeException {
+    public LowBalanceException() {
+        super();
+    }
+
+    public LowBalanceException(String message) {
+        super(message);
+    }
+}
