4a168db7ca5f934177b053bcaf62195050a989e3
Shashank Hegde <141432130+shashankhegde06@users.noreply.github.com>
2024-12-12 15:07:22 +0530

    Delete repy

--- src/main/java/com/alien/bank/management/system/repository/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alien.bank.management.system.repository;
-
-import com.alien.bank.management.system.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
