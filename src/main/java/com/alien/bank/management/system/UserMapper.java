8a6fc578de4c51d1c57896ebac9b8d10e069085c
Shashank Hegde <141432130+shashankhegde06@users.noreply.github.com>
2024-12-12 14:57:53 +0530

    Delete src/main/java/com/alien/bank/management/system/UserMapper.java

--- src/main/java/com/alien/bank/management/system/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.alien.bank.management.system.mapper;
-
-import com.alien.bank.management.system.entity.User;
-import com.alien.bank.management.system.model.authentication.RegisterRequestModel;
-
-public interface UserMapper {
-    User toUser(RegisterRequestModel request);
-}
