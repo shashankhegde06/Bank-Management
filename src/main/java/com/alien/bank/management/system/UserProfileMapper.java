c1c5d8ae6505b93722abff7fb94ec551e9913936
Shashank Hegde <141432130+shashankhegde06@users.noreply.github.com>
2024-12-12 14:57:18 +0530

    Delete

--- src/main/java/com/alien/bank/management/system/UserProfileMapper.java
+++
@@ -1,8 +0,0 @@
-package com.alien.bank.management.system.mapper;
-
-import com.alien.bank.management.system.entity.User;
-import com.alien.bank.management.system.model.authentication.UserProfileResponseModel;
-
-public interface UserProfileMapper {
-    UserProfileResponseModel toUserProfile(User user);
-}
